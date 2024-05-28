7c8664e572830bb25ae576fe7de8f5fec05c3358
Hidenori_809 Suzuki <suzuki809hidenori@gmail.com>
2024-05-29 04:46:00 +0900

    lesson86-finished:動画とRails7の互換性？対応でturbo等をインストール

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,8 +1,45 @@
 class BoardsController < ApplicationController
+  before_action :set_target_board, only: %i[show edit update destroy]
   def index
+    @boards = Board.all
   end
 
   def new
+    @board = Board.new
+    # binding.pry
   end
 
+  def create
+    board = Board.create(board_params)
+    redirect_to board
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @board.update(board_params)
+
+    redirect_to @board
+  end
+
+  def destroy
+    @board.destroy
+
+    redirect_to boards_path
+  end
+
+
+  private
+
+  def board_params
+    params.require(:board).permit(:author_name, :title, :body)
+  end
+
+  def set_target_board
+    @board = Board.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :boards
   # Defines the root path route ("/")
-  get 'boards', to: 'boards#index'
-  get 'boards/new', to: 'boards#new'
+  # get 'boards', to: 'boards#index'
+  # get 'boards/new', to: 'boards#new'
+  # post 'boards', to: 'boards#create'
+  # get 'boards/:id', to: 'boards#show'
 end
