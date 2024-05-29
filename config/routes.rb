eda45d905381074f4fbb87198f3065d34df0809e
Hidenori_809 Suzuki <suzuki809hidenori@gmail.com>
2024-05-30 05:24:21 +0900

    Add：コメント機能を作成中_9-96

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :boards
+  resources :comments, only: %i[create destroy]
   # Defines the root path route ("/")
   # get 'boards', to: 'boards#index'
   # get 'boards/new', to: 'boards#new'
